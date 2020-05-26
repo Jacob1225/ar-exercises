adf8a1d6bbfb04e574c719f44076fdc08692fcf8
Jacob1225 <carlone.jacob@gmail.com>
2020-05-26 15:43:59 +0000

    displays the sum & average for total revenue of all stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,17 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum = Store.sum(:annual_revenue)
+puts "The total revenue for all stores is #{@sum}"
+
+@average = Store.average(:annual_revenue)
+puts "The total average revenue for all stores is #{@average}"
+
+@profitable_stores = Store.where("annual_revenue > ?", 1000000).count
+
+if @profitable_stores  == 1
+    puts "#{@profitable_stores} store is generating more the 1M in sales"
+
+else 
+    puts "#{@profitable_stores} stores are generating more the 1M in sales" 
+end
